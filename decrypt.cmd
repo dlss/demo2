c0a3931fad616449a06a0f7e8322926a39f3de0d
dlss <309964264@qq.com>
2017-03-29 12:46:42 +0800

    update

--- decrypt.cmd
+++ decrypt.cmd
@@ -1,11 +1,11 @@
 @ECHO OFF
 set en_suffix=_encrypt
 
-For /f "eol=# tokens=1* delims=:" %%i in (encryptinfo.txt) do (
-If "%%i"=="key" set key=%%j
-If "%%i"=="iv" set iv=%%j
-If "%%i"=="method" set alt=%%j
-If "%%i"=="encrypttool" set encryptTool=%%j
+For /f "eol=# tokens=1* delims==" %%i in (encryptinfo.txt) do (
+If "%%i"=="key" set key="%%j"
+If "%%i"=="iv" set iv="%%j"
+If "%%i"=="alt" set alt="%%j"
+If "%%i"=="ep" set encryptTool="%%j"
 )
 
 setlocal EnableDelayedExpansion
@@ -18,7 +18,7 @@ for /f "eol=#" %%i in (encryptlist.txt) do (
 		) else (
 			if exist "!path!%en_suffix%" (
 				echo decrypt "!path!%en_suffix%"
-			    %encryptTool% -1 -de -f "!path!%en_suffix%" -t %alt% -key !key! -iv !iv! -o "!path!" -ver 0
+			    %encryptTool% -1 -de -file "!path!%en_suffix%" -alt %alt% -key !key! -iv !iv! -out "!path!" -ver 0
 			)
 		)
 	) else echo not exist "!path!"
@@ -29,7 +29,7 @@ goto :eof
 for /R %1 %%a in (*) do (
 	if exist "%%a%en_suffix%" (
 		echo decrypt "%%a%en_suffix%"
-		%encryptTool% -1 -de -f "%%a%en_suffix%" -t %alt% -key !key! -iv !iv! -o "%%a" -ver 0
+		%encryptTool% -1 -de -file "%%a%en_suffix%" -alt %alt% -key !key! -iv !iv! -out "%%a" -ver 0
 	)
 )
 
--- encrypt.cmd
+++ encrypt.cmd
@@ -1,11 +1,11 @@
 @ECHO OFF
 set en_suffix=_encrypt
 
-For /f "eol=# tokens=1* delims=:" %%i in (encryptinfo.txt) do (
-If "%%i"=="key" set key=%%j
-If "%%i"=="iv" set iv=%%j
-If "%%i"=="method" set alt=%%j
-If "%%i"=="encrypttool" set encryptTool=%%j
+For /f "eol=# tokens=1* delims==" %%i in (encryptinfo.txt) do (
+If "%%i"=="key" set key="%%j"
+If "%%i"=="iv" set iv="%%j"
+If "%%i"=="alt" set alt="%%j"
+If "%%i"=="ep" set encryptTool="%%j"
 )
 
 setlocal EnableDelayedExpansion
@@ -18,7 +18,7 @@ for /f "eol=#" %%i in (encryptlist.txt) do (
 		) else (
 			if exist "!path!" (
 				echo encrypt "!path!"
-			    %encryptTool% -1 -en -f "!path!" -t %alt% -key !key! -iv !iv! -o "!path!%en_suffix%" -ver 0
+			    %encryptTool% -1 -en -file "!path!" -alt %alt% -key !key! -iv !iv! -out "!path!%en_suffix%" -ver 0
 			) else echo Not exist "!path!" 
 		)
 	) else echo Not exist "!path!" 
@@ -30,7 +30,7 @@ for /R %1 %%a in (*) do (
 	set file=%%a
 	if "!file:%en_suffix%=!"=="!file!" (
 		echo encrypt "%%a"
-		%encryptTool% -1 -en -f "%%a" -t %alt% -key !key! -iv !iv! -o "%%a%en_suffix%" -ver 0
+		%encryptTool% -1 -en -file "%%a" -alt %alt% -key !key! -iv !iv! -out "%%a%en_suffix%" -ver 0
 	) else echo skip to encrypt "%%a"
 )
 
